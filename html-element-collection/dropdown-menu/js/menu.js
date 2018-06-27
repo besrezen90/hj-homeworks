812613f3c8e829b62fa580862acc6e2388739bf5
Aleksandr Berezin <besrezen90@mail.ru>
2018-06-27 18:28:26 +0300

    Исправил toggle

--- html-element-collection/dropdown-menu/js/menu.js
+++ html-element-collection/dropdown-menu/js/menu.js
@@ -2,9 +2,7 @@
 let buttonMenu = document.getElementsByClassName("wrapper-dropdown");
 
 function dropdown() {
-
-    if(this.classList.toggle("active")) this.classList.add("active")
-    else this.classList.remove("active")
+    this.classList.toggle("active")
 }
 
 for (let button of buttonMenu) {
