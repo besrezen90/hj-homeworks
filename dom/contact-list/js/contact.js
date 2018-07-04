4772328e1d6cd9f5933e3bc7c3c6cfc0616245de
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-04 18:30:10 +0300

    Задача 1

---
+++ dom/contact-list/js/contact.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let newList = document.querySelector(".contacts-list"),
+    oldListItem = document.querySelector(".contacts-list > li");
+newList.removeChild(oldListItem);
+
+
+function parseNewContactList() {
+    let newContactList = JSON.parse(loadContacts())
+    newContactList.forEach(function (newContact) {
+        let newListItem = document.createElement('li');
+        newListItem.dataset.email = newContact.email
+        newListItem.dataset.phone = newContact.phone
+        newListItem.innerHTML = `<strong>${newContact.name}`
+        newList.appendChild(newListItem);
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', parseNewContactList);
