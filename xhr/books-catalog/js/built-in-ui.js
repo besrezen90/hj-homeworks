20c70661a466333bfdcc02d979afe3c7d5436f41
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-07 14:47:22 +0300

    Задача 3

--- xhr/books-catalog/js/built-in-ui.js
+++ xhr/books-catalog/js/built-in-ui.js
@@ -1,9 +1,9 @@
 /* Данный JS код */
 
 // Регулируем видимость карточки
-function toggleCardVisible () {
- document.getElementById('content').classList.toggle('hidden');
- document.getElementById('card').classList.toggle('hidden');
+function toggleCardVisible() {
+    document.getElementById('content').classList.toggle('hidden');
+    document.getElementById('card').classList.toggle('hidden');
 }
 
 
@@ -19,10 +19,37 @@ document.getElementById('content').addEventListener('click', (event) => {
     }
 
     if (target) {
-      toggleCardVisible();
-      document.getElementById('card-title').innerHTML = target.dataset.title;
-      document.getElementById('card-author').innerHTML = target.dataset.author;
-      document.getElementById('card-info').innerHTML = target.dataset.info;
-      document.getElementById('card-price').innerHTML = target.dataset.price;
+        toggleCardVisible();
+        document.getElementById('card-title').innerHTML = target.dataset.title;
+        document.getElementById('card-author').innerHTML = target.dataset.author;
+        document.getElementById('card-info').innerHTML = target.dataset.info;
+        document.getElementById('card-price').innerHTML = target.dataset.price;
     }
 });
+// Запрос библиотеки
+
+let content = document.getElementById("content"),
+    oldContent = document.querySelector("#content li")
+
+function parseNewBooktList() {
+    content.removeChild(oldContent)
+    let catalog = new XMLHttpRequest()
+    catalog.addEventListener('load', onLoad)
+    catalog.open("GET", "https://neto-api.herokuapp.com/book/")
+    catalog.send()
+
+    function onLoad() {
+        let newCatalog = JSON.parse(catalog.responseText)
+        newCatalog.forEach(function (newCatalogItem) {
+            let newListItem = document.createElement('li');
+            newListItem.dataset.title = newCatalogItem.title
+            newListItem.dataset.author = newCatalogItem.author.name
+            newListItem.dataset.info = newCatalogItem.info
+            newListItem.dataset.price = newCatalogItem.price
+            newListItem.innerHTML = `<img src=${newCatalogItem.cover.small}>`
+            content.appendChild(newListItem)
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', parseNewBooktList);
