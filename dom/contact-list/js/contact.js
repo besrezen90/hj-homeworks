bae27a9c434a0463b99e1f48a33c959d86fb2f6b
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-10 20:24:30 +0300

    правки

--- dom/contact-list/js/contact.js
+++ dom/contact-list/js/contact.js
@@ -6,12 +6,12 @@ newList.removeChild(oldListItem);
 
 
 function parseNewContactList() {
-    let newContactList = JSON.parse(loadContacts())
+    let newContactList = JSON.parse(loadContacts());
     newContactList.forEach(function (newContact) {
         let newListItem = document.createElement('li');
-        newListItem.dataset.email = newContact.email
-        newListItem.dataset.phone = newContact.phone
-        newListItem.innerHTML = `<strong>${newContact.name}`
+        newListItem.dataset.email = newContact.email;
+        newListItem.dataset.phone = newContact.phone;
+        newListItem.innerHTML = `<strong>${newContact.name}</strong>`;
         newList.appendChild(newListItem);
     })
 }
