b3a21bd9d4d4a55d086b0f1a98bcdc9f42319cbb
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-10 20:17:39 +0300

    правки

--- html-forms/currency-converter/js/script.js
+++ html-forms/currency-converter/js/script.js
@@ -4,46 +4,46 @@ const currencyList = new XMLHttpRequest();
 let content = document.getElementById("content");
 
 currencyList.addEventListener("loadstart", LoadStart);
-currencyList.addEventListener("load", onLoad)
-currencyList.addEventListener("loadend", loadEnd)
-currencyList.open("GET", "https://neto-api.herokuapp.com/currency")
-currencyList.send()
+currencyList.addEventListener("load", onLoad);
+currencyList.addEventListener("loadend", loadEnd);
+currencyList.open("GET", "https://neto-api.herokuapp.com/currency");
+currencyList.send();
 
 
 function LoadStart() {
-    document.getElementById("loader").classList.remove("hidden")
+    document.getElementById("loader").classList.remove("hidden");
 }
 
 function onLoad() {
-    let listForm = content.querySelectorAll("select")
-    const newCurList = JSON.parse(currencyList.responseText)
+    let listForm = content.querySelectorAll("select");
+    const newCurList = JSON.parse(currencyList.responseText);
     for (let i = 0; i < listForm.length; i++) {
         Array.from(newCurList).forEach(item => {
             let elem = document.createElement("option")
-            elem.innerText = item.code
-            elem.value = item.value
-            elem.dataset.name = item.title
-            listForm[i].appendChild(elem)
-        })
+            elem.innerText = item.code;
+            elem.value = item.value;
+            elem.dataset.name = item.title;
+            listForm[i].appendChild(elem);
+        });
 
     }
-    calc()
+    calc();
 }
 
 function loadEnd() {
-    document.getElementById("loader").classList.add("hidden")
-    content.classList.remove("hidden")
+    document.getElementById("loader").classList.add("hidden");
+    content.classList.remove("hidden");
 }
 
 let count = document.getElementById("source"),
     countFrom = document.getElementById("from"),
-    countTo = document.getElementById("to")
+    countTo = document.getElementById("to");
 
 function calc() {
-    document.getElementById("result").innerText = (count.value * countFrom.value / countTo.value).toFixed(2)
+    document.getElementById("result").innerText = (count.value * countFrom.value / countTo.value).toFixed(2);
 }
 
-count.addEventListener('change', calc)
-countFrom.addEventListener('change', calc)
-countTo.addEventListener('change', calc)
+count.addEventListener('change', calc);
+countFrom.addEventListener('change', calc);
+countTo.addEventListener('change', calc);
 
--- html-forms/todo-list/js/list.js
+++ html-forms/todo-list/js/list.js
@@ -2,19 +2,19 @@
 let list = document.querySelector(".list-block"),
     listItem = list.querySelectorAll("li"),
     input = list.querySelectorAll("li input"),
-    output = list.querySelector("h3 output"),
+    output = list.querySelector("h3 output");
 
 
 function toDoShow() {
     let startCount = Array.from(input).filter(elem => {
-        return elem.checked === true
+        return elem.checked === true;
     })
-    output.innerText = `${startCount.length} из ${listItem.length}`
-    if(startCount.length === listItem.length) list.classList.add("complete")
-    else list.classList.remove("complete")
+    output.innerText = `${startCount.length} из ${listItem.length}`;
+    if(startCount.length === listItem.length) list.classList.add("complete");
+    else list.classList.remove("complete");
 
 }
 
-document.addEventListener('DOMContentLoaded', toDoShow)
-Array.from(input).forEach(elem => elem.addEventListener('change', toDoShow))
+document.addEventListener('DOMContentLoaded', toDoShow);
+Array.from(input).forEach(elem => elem.addEventListener('change', toDoShow));
 
