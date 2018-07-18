fce325b70cdafe8deb192983883438e6afa71ce1
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-18 22:19:29 +0300

    Задача 3

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function controlBuy(container) {
+
+    let buttons = container.querySelectorAll('.add-to-cart');
+    function tabButton(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const item = {
+            title: event.target.dataset.title,
+            price: event.target.dataset.price
+        }
+        addToCart(item)
+    }
+
+    Array.from(buttons).forEach(button => button.addEventListener('click', tabButton));
+
+    container.addEventListener('DOMNodeInserted', function(event){
+        event.target.querySelector('.add-to-cart').addEventListener('click', tabButton)
+    })
+}
+
+
+const itemList = document.querySelector('.items-list');
+
+document.addEventListener('DomContentLoaded', controlBuy(itemList))
