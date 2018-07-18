f1cf332d0e501379698da91fff338f75a741617c
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-18 20:47:03 +0300

    Задача 2

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+
+    const titles = table.querySelectorAll('th.prop__name');
+
+    Array.from(titles).forEach(function (elem) {
+        if (!(elem === event.target)) return;
+
+        if (event.target.dataset.dir) {
+            event.target.dataset.dir = new Number(event.target.dataset.dir) * (-1);
+        } else event.target.dataset.dir = 1;
+        table.dataset.sortBy = event.target.dataset.propName;
+
+        sortTable(table.dataset.sortBy, event.target.dataset.dir);
+    })
 }
