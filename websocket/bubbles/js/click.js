d9fbffda6ad09313c4fbc4190d55faeb5aab0e1f
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-22 14:41:50 +0300

    Задача 1

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,18 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+    connection.addEventListener('open', function () {
+        showBubbles(connection);
+    })
+
+    window.addEventListener('click', showClick);
+
+    function showClick(event) {
+        connection.send(JSON.stringify({
+            x: `${event.x}`,
+            y: `${event.y}`
+        }));
+    }
+
+})
