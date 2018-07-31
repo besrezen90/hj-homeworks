cdfe01e1ab99bea5392ce3c2e2fed11736a80f4e
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-31 20:11:06 +0300

    Задача 3

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,35 @@
 'use strict';
+
+
+function createElement(block) {
+    if ((block === undefined) || (block === null) || (block === false)) {
+        return document.createTextNode('');
+    }
+    if ((typeof block === 'string') || (typeof block === 'number') || (block === true)) {
+        return document.createTextNode(block.toString());
+    }
+    if (Array.isArray(block)) {
+        return block.reduce((f, elem) => {
+            f.appendChild(createElement(elem));
+
+            return f;
+        }, document.createDocumentFragment());
+    }
+
+    const element = document.createElement(block.name || 'div');
+
+    [].concat(block.cls || []).forEach(
+        className => element.classList.add(className)
+    );
+
+    if (block.props) {
+        Object.keys(block.props).forEach(
+            key => element.setAttribute(key, block.props[key])
+        );
+    }
+
+    if (block.childs) element.appendChild(createElement(block.childs));
+
+    return element;
+}
+
