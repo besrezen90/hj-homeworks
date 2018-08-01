1ac1ae55926a2f401f026aea836ff38204bf223c
Aleksandr Berezin <besrezen90@mail.ru>
2018-08-01 20:42:39 +0300

    Задача 1

--- comet/realtime/js/realtime.js
+++ comet/realtime/js/realtime.js
@@ -12,18 +12,18 @@ const realtime = new Chart(ctx).Bar({
 });
 
 let isFirst = true;
+
 const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
+
 ws.addEventListener('message', event => {
+  console.log(JSON.parse(event.data))
   if (isFirst) {
-    event.data
-      .split('\n')
-      .map(line => line.split('|'))
-      .forEach(data => realtime.addData([Number(data[1])], data[0]));
+    JSON.parse(event.data)
+      .forEach(data => realtime.addData([Number(data.online)], data.time));
 
     isFirst = false;
   } else {
-    const [label, data] = event.data.split('|');
     realtime.removeData();
-    realtime.addData([Number(data)], label);
+    realtime.addData([Number(JSON.parse(event.data).online)], JSON.parse(event.data).time);
   }
 });
