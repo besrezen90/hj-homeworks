bdc459efee6423593390eeb872d19d34ac84621d
Aleksandr Berezin <besrezen90@mail.ru>
2018-08-02 20:11:13 +0300

    Задача 2

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+
+
+ws.addEventListener('open', () => {
+    console.log('Соединение открыто');
+})
+
+window.editor.addEventListener('update', event => {
+    event.canvas.toBlob(function(blob){
+        ws.send(blob);
+    })
+})
