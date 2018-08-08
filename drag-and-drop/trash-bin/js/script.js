787d327792d80bd78384ad86f281acb7bd60d9f5
Aleksandr Berezin <besrezen90@mail.ru>
2018-08-08 19:44:53 +0300

    Задача 1

---
+++ drag-and-drop/trash-bin/js/script.js
@@ -0,0 +1,46 @@
+'use strict'
+
+let movedPiece = null;
+let oldCoord = null;
+
+document.addEventListener('mousedown', event => {
+    if (event.target.classList.contains('logo')) {
+        oldCoord = {
+            x: event.target.getBoundingClientRect().left,
+            y: event.target.getBoundingClientRect().top
+        };
+        movedPiece = event.target;
+        movedPiece.classList.add('moving');
+    }
+});
+
+document.addEventListener('mousemove', event => {
+    if (movedPiece) {
+        // Предотвращаем выделение текста
+        event.preventDefault();
+        movedPiece.style.left = `${event.pageX - (movedPiece.width / 2)}px`;
+        movedPiece.style.top = `${event.pageY - (movedPiece.height / 2)}px`;
+
+    }
+    else movedPiece = null;
+});
+
+document.addEventListener('mouseup', event => {
+    if (movedPiece) {
+        movedPiece.classList.remove('moving');
+        movedPiece.style.visibility = 'hidden';
+        const trashBin = document.elementFromPoint(event.clientX, event.clientY).closest('#trash_bin');
+        movedPiece.style.visibility = 'visible';
+        if (trashBin) {
+            movedPiece.style.display = 'none';
+            movedPiece = null;
+            oldCoord = null;
+        }  else {
+            movedPiece.style.left = `${oldCoord.x}px`;
+            movedPiece.style.top = `${oldCoord.y}px`;
+            oldCoord = null;
+            movedPiece = null;
+        }
+    }
+});
+
