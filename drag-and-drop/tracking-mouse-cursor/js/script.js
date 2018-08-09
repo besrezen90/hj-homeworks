4a3bcd394a0f3e11964207ef3940ba79f7a5112e
Aleksandr Berezin <besrezen90@mail.ru>
2018-08-09 19:35:47 +0300

    Задача 2

--- drag-and-drop/tracking-mouse-cursor/js/script.js
+++ drag-and-drop/tracking-mouse-cursor/js/script.js
@@ -7,16 +7,14 @@ let coord = null;
 
 function catEye(eye, coord) {
     const pupil = eye.querySelector('.cat_eye');
-    pupil.style.transition = 'none';
 
-    const coef = {
-        x: (eye.getBoundingClientRect().right - eye.getBoundingClientRect().left + pupil.clientWidth) / document.documentElement.clientWidth,
-        y: (eye.getBoundingClientRect().bottom - eye.getBoundingClientRect().top + pupil.clientHeight) / document.documentElement.clientHeight
-    }
-
-    pupil.style.left = `${coord.x * coef.x}%`;
-    pupil.style.top = `${coord.y * coef.y}%`;
-
+  if(coord.x < eye.getBoundingClientRect().left) pupil.style.left = `${0}%`
+  if(coord.x > eye.getBoundingClientRect().right) pupil.style.left = `${50}%`
+  if(eye.getBoundingClientRect().left < coord.x && coord.x < eye.getBoundingClientRect().right) pupil.style.left = `${25}%`
+  if(coord.y < eye.getBoundingClientRect().top) pupil.style.top = `${0}%`
+  if(coord.y > eye.getBoundingClientRect().bottom) pupil.style.top = `${50}%`
+  if(eye.getBoundingClientRect().top < coord.y && coord.y < eye.getBoundingClientRect().bottom) pupil.style.top = `${25}%`
+  
 }
 
 
